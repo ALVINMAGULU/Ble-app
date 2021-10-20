909dc3ad7ef2a508727604ea7d1e0c5448049f17
Almag25 <magulualvin@gmail.com>
2021-10-20 11:57:08 +0300

    removed message

--- ESP32 ble program/ESP32_BLE/ESP32_BLE.ino
+++ ESP32 ble program/ESP32_BLE/ESP32_BLE.ino
@@ -7,8 +7,6 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
 
 #define SERVICE_UUID        "ab0828b1-198e-4351-b779-901fa0e0371e"
 #define CHARACTERISTIC_UUID_TX "4ac8a682-9736-4e5d-932b-e9b31405049c"
